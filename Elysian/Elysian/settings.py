5500a347f5ac1e092a6c04b97fe7577712b7aebc
Lakshyaquaere <lakshyaquaere@gmail.com>
2025-08-11 17:54:01 +0530

    update

--- Elysian/Elysian/settings.py
+++ Elysian/Elysian/settings.py
@@ -1,39 +1,26 @@
 from pathlib import Path
 import os
+from mongoengine import connect
 
-# Import MongoDB connection
-import Elysian.db  # This runs connect() at startup
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# MongoDB connection
+MONGO_URI = "mongodb+srv://ajay:bGptBVJS5Cem3pek@cluster0.lkwz9i8.mongodb.net/your_db_name"
+connect(host=MONGO_URI)
+
 SECRET_KEY = 'django-insecure-q58&zeaq3-k!bi5b_ur&ht+o_pbn&jcl*=!w#&6nn+u%pwg@bn'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
-# Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Add your apps here, e.g. 'products',
+    # Remove apps you don’t need from Django's ORM
+    'django.contrib.staticfiles',  # keep if you serve static files
+    # Add your custom apps here
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'Elysian.urls'
@@ -46,8 +33,6 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
@@ -55,20 +40,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Elysian.wsgi.application'
 
-# Keep SQLite for Django's built-in apps
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
+# Remove default DATABASES since MongoEngine is being used
+DATABASES = {}
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
@@ -76,4 +49,3 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
